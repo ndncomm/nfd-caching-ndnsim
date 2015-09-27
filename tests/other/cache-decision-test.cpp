2e3979cbe2361d9005257bb2a842c521daa857be
schneiderklaus <klaus@email.arizona.edu>
2015-09-27 10:51:31 -0700

    Added first simulation + results

--- tests/other/cache-decision-test.cpp
+++ tests/other/cache-decision-test.cpp
@@ -30,39 +30,58 @@
 #include "../../NFD/daemon/table/cs-lcd-decision-policy.hpp"
 
 #include <ns3/node-list.h>
+#include <cassert>
+#include <iterator>
+#include "../../NFD/daemon/table/cs-decision-policy.hpp"
+#include "../../utils/tracers/ndn-app-delay-tracer.hpp"
+#include "../../utils/tracers/ndn-cs-tracer.hpp"
+#include <ns3-dev/ns3/node-container.h>
+#include <ns3-dev/ns3/point-to-point-helper.h>
 
 namespace ns3 {
 
 void run(int argc, char* argv[])
 {
-//  CommandLine cmd;
-//  std::string params = "";
-//  std::string strategy = "lowest-cost";
-//  cmd.AddValue("params", "Number of packets to echo", params);
-//  cmd.AddValue("strategy", "Forwarding strategy to use", strategy);
-//  cmd.Parse(argc, argv);
 
-  ScenarioHelper helper;
+//  ScenarioHelper helper;
+//  helper.createTopology(
+//      { { "client", "r1" }, { "r1", "r2" }, { "r2", "r3" }, { "r3", "producer" } });
+//  helper.addRoutes( { { "client", "r1", "/", 1 }, { "r1", "r2", "/", 1 }, { "r2", "r3", "/", 1 }, {
+//      "r3", "producer", "/", 1 }, });
 
-  helper.createTopology(
-      { { "client", "r1" }, { "r1", "r2" }, { "r2", "r3" }, { "r3", "producer" } });
+// Creating nodes
+  NodeContainer nodes;
+  nodes.Create(5);
 
-  helper.addRoutes( { { "client", "r1", "/", 1 }, { "r1", "r2", "/", 1 }, { "r2", "r3", "/", 1 }, {
-      "r3", "producer", "/", 1 }, });
+  // Connecting nodes using two links
+  PointToPointHelper p2p;
+  p2p.Install(nodes.Get(0), nodes.Get(1));
+  p2p.Install(nodes.Get(1), nodes.Get(2));
+  p2p.Install(nodes.Get(2), nodes.Get(3));
+  p2p.Install(nodes.Get(3), nodes.Get(4));
 
-  Ptr<Node> client = Names::Find<Node>("client");
-  Ptr<Node> r1 = Names::Find<Node>("r1");
-  Ptr<Node> r2 = Names::Find<Node>("r2");
-  Ptr<Node> r3 = Names::Find<Node>("r3");
-  Ptr<Node> producer = Names::Find<Node>("producer");
+  // Install NDN stack on all nodes
+  ndn::StackHelper ndnHelper;
+  ndnHelper.SetOldContentStore("ns3::ndn::cs::Lru", "MaxSize", "100"); // default ContentStore parameters
+  ndnHelper.SetDefaultRoutes(true);
+  ndnHelper.InstallAll();
+
+  Ptr<Node> client = nodes.Get(0);
+  Ptr<Node> r1 = nodes.Get(1);
+  Ptr<Node> r2 = nodes.Get(2);
+  Ptr<Node> r3 = nodes.Get(3);
+  Ptr<Node> producer = nodes.Get(4);
 
 //  nfd::Forwarder clientForwarder
   shared_ptr<nfd::Forwarder> clientForwarder = client->GetObject<ndn::L3Protocol>()->getForwarder();
 
+  int ACCEPT_RATIO = 50;
+  std::string SIM_NAME = "ar" + std::to_string(ACCEPT_RATIO);
+
   for (NodeList::Iterator node = NodeList::Begin(); node != NodeList::End(); node++) {
     shared_ptr<nfd::Forwarder> forwarder = (*node)->GetObject<ndn::L3Protocol>()->getForwarder();
     auto policy = make_shared<nfd::cs::UniformDecisionPolicy>(
-        nfd::cs::UniformDecisionPolicy(95));
+        nfd::cs::UniformDecisionPolicy(ACCEPT_RATIO));
 
     forwarder->setDecisionPolicy(policy);
     std::cout << forwarder->getDecisionPolicy()->getName() << ": ";
@@ -71,8 +90,7 @@ void run(int argc, char* argv[])
     std::cout << unifPol->getAcceptRatio() << "\n";
 
     // Correct acceptance rate:
-    assert(unifPol->getAcceptRatio() == 95);
-
+    assert(unifPol->getAcceptRatio() == ACCEPT_RATIO);
   }
 
 // Installing applications //
@@ -82,21 +100,19 @@ void run(int argc, char* argv[])
   consumerHelper1.SetAttribute("StopTime", StringValue("10"));
   consumerHelper1.Install(client);
 
-
 // Producer
   AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix("/prefix");
   producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
   producerHelper.Install(producer);
 
-//  TestHelper::setLinkError(client->GetDevice(0), 0.1);
-
-//  Simulator::Schedule(Seconds(13.0), TestHelper::printFaceCounter, faceVector);
+  std::string folder = "src/ndnSIM/results/";
+  AppDelayTracer::InstallAll(folder + SIM_NAME + "app-delays-trace.txt");
+  CsTracer::InstallAll(folder + SIM_NAME + "cs-trace.txt", Seconds(1));
 
   Simulator::Stop(Seconds(15.0));
   Simulator::Run();
   Simulator::Destroy();
-
 }
 
 }
