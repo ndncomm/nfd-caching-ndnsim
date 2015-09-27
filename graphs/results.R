a5b152379f7368a6df15872ea59baec1182d1385
schneiderklaus <klaus@email.arizona.edu>
2015-09-27 13:25:34 -0700

    update

--- graphs/results.R
+++ graphs/results.R
@@ -5,39 +5,50 @@ library(plyr)
 setwd("~/ccn/ndnSIM2.1/ns-3/src/ndnSIM/graphs")
 
 cs100 <-  read.table("../results/ar100cs-trace.txt", header=T)
-cs25 <-  read.table("../results/ar25cs-trace.txt", header=T)
+cs80 <-  read.table("../results/ar80cs-trace.txt", header=T)
+cs60 <-  read.table("../results/ar60cs-trace.txt", header=T)
+cs40 <-  read.table("../results/ar40cs-trace.txt", header=T)
+cs20 <-  read.table("../results/ar20cs-trace.txt", header=T)
+
+# cs25 <-  read.table("../results/ar25cs-trace.txt", header=T)
 
 app100 <-  read.table("../results/ar100app-trace.txt", header=T)
-app25 <-  read.table("../results/ar25app-trace.txt", header=T)
+app80 <-  read.table("../results/ar80app-trace.txt", header=T)
+app60 <-  read.table("../results/ar60app-trace.txt", header=T)
+app40 <-  read.table("../results/ar40app-trace.txt", header=T)
+app20 <-  read.table("../results/ar20app-trace.txt", header=T)
 
 cs100$Node <- factor(cs100$Node)
-cs25$Node <- factor(cs25$Node)
+cs80$Node <- factor(cs80$Node)
+cs60$Node <- factor(cs60$Node)
+cs40$Node <- factor(cs40$Node)
+cs20$Node <- factor(cs20$Node)
+
 app100$Node <- factor(app100$Node)
-app25$Node <- factor(app25$Node)
-
-summary(cs100)
-summary(cs25)
-
-cs100$Perc=100
-cs25$Perc=25
-
+app80$Node <- factor(app80$Node)
+app60$Node <- factor(app60$Node)
+app40$Node <- factor(app40$Node)
+app20$Node <- factor(app20$Node)
 
 summary(app100)
 summary(app25)
 
-
-
-
 cs100.hits <- subset(cs100, Type=="CacheHits")
-cs25.hits <- subset(cs25, Type=="CacheHits")
+cs80.hits <- subset(cs80, Type=="CacheHits")
+cs60.hits <- subset(cs60, Type=="CacheHits")
+cs40.hits <- subset(cs40, Type=="CacheHits")
+cs20.hits <- subset(cs20, Type=="CacheHits")
 
-delays <- melt(data.frame(app100$HopCount,app25$DelayS))
-colnames(delays) <- c("Type", "Packets")
+summary(app60)
+summary(app20)
+summary(app80)
 
-hopcounts <- melt(data.frame(app100$HopCount,app25$HopCount))
+app100$HopCount[1:1978]
+
+hopcounts <- melt(data.frame(app100$HopCount,app80$HopCount,app60$HopCount,app40$HopCount))
 colnames(hopcounts) <- c("Type", "Packets")
 
-hits <- melt(data.frame(cs100.hits$Packets,cs25.hits$Packets))
+hits <- melt(data.frame(cs100.hits$Packets,cs80.hits$Packets,cs60.hits$Packets,cs40.hits$Packets,cs20.hits$Packets))
 colnames(hits) <- c("Type", "Packets")
 summary(hits)
 
@@ -45,7 +56,6 @@ ggplot(hits, aes(x=Type, y=Packets)) +
   geom_boxplot() +
   ylab("Avg. Cache Hits [/s]")
 
-hopcounts
 
 ggplot(hopcounts, aes(x=Type, y=Packets)) + 
   geom_boxplot() +
@@ -61,7 +71,7 @@ min.mean.sd.max <- function(x) {
 ggplot(hopcounts, aes(x = Type, y=Packets)) + 
   #stat_summary(fun.data = min.mean.sd.max, geom = "boxplot") + 
   geom_jitter(position=position_jitter(width=.2), size=3) 
-
+ 
 # ggplot(delays, aes(x=Type, y=Packets)) + 
 #   geom_boxplot() +
 #   ylab("Avg. Delay [s]")
--- tests/integration-tests.sh
+++ tests/integration-tests.sh
@@ -2,10 +2,18 @@
 
 NS3_DIR="/home/klaus/ccn/ndnSIM2.1/ns-3"
 
+if [ ${#1} == 0 ]
+then
+      PAR="100"
+	else
+	  PAR=$1
+fi
+
+
 cd $NS3_DIR
 
 echo ""
 echo "Running simulations"
-./waf --run="cache-decision-simulation"
+./waf --run="cache-decision-simulation --params=$PAR"
 
 echo "Finished!"
